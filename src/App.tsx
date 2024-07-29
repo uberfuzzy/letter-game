574e865097d2739db1d8b44f53ff13096e5f56e3
Uberfuzzy <i.am@uberfuzzy.com>
2024-07-29 16:03:39 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState, useEffect } from 'react';
+
 import './App.css'
+import { GuessHistory } from './components/GuessHistory';
 
 function App() {
+  const [words, setWords] = useState<string[]>([]);
+  const [randomWord, setRandomWord] = useState<string>('');
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [winState, setWinState] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchAndSetWords = async () => {
+      const words = await fetchWords('./words.txt');
+      setWords(words);
+    };
+    fetchAndSetWords();
+  }, []);
+
+  const handleGetRandomWord = () => {
+    if (words.length > 0) {
+      setRandomWord(getRandomWord(words).toLocaleLowerCase());
+      setInputValue("");
+      setGuesses([]);
+      setWinState(false);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value.toLocaleLowerCase());
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      if (inputValue.length === 5) {
+        // add to history
+        setGuesses(guesses => [...guesses, inputValue]);
+        // clear it
+        setInputValue("");
+        if (randomWord.toLocaleLowerCase() === inputValue.toLocaleLowerCase()) {
+          setWinState(true);
+        }
+      } else {
+        window.alert("enter exactly 5 letters")
+      }
+    }
+  };
 
   return (
     <>
-      <h1>Letter game</h1>
-      <div id="board"></div>
+      <div id="controls">
+        <button onClick={handleGetRandomWord}>Get Random Word</button>
+        {randomWord && <p>Random Word: <span className={`secretWord ${winState ? '' : "spoiler"}`} title={randomWord}>{randomWord.toLocaleUpperCase()}</span></p>}
+      </div>
+      <div>
+        {randomWord && !winState && (
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            disabled={winState}
+            pattern="[A-Za-z]{5}"
+          />
+        )}
+      </div>
+      <GuessHistory current={randomWord} thepast={guesses} />
     </>
   )
 }
 
 export default App
+
+async function fetchWords(filePath: string): Promise<string[]> {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const text = await response.text();
+    return text.split('\n').map(word => word.trim()).filter(word => word.length > 0);
+  } catch (error) {
+    console.error('Error fetching and parsing the file:', error);
+    return [];
+  }
+}
+
+function getRandomWord(words: string[]): string {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex];
+}
---
+++ src/components/GuessHistory.tsx
@@ -0,0 +1,61 @@
+const yesClass = 'correct_exact';
+const maybeClass = 'correct_wrong';
+const noClass = 'incorrect';
+
+import "./GuessHistory.css";
+
+export function GuessHistory({ current, thepast }: { current: string, thepast: string[] }) {
+  const rows: JSX.Element[] = [];
+
+  thepast.forEach((guess, guessId) => {
+    const isMatch = guess.toLocaleLowerCase() === current.toLocaleLowerCase();
+
+    const currentLetters = current.split("");
+    const guessLetters = guess.split("");
+
+    // make some slots, default to all wrong
+    const matchMap: number[] = new Array(current.length).fill(0);
+
+    if (isMatch) {
+      // is the same, just set all to match
+      matchMap.fill(2);
+    } else {
+      // was not match, do some work
+
+      guessLetters.forEach((gl, index) => {
+        if (gl.localeCompare(currentLetters[index], undefined, { sensitivity: "base" }) === 0) {
+          // letter is right in right place, mark it as good
+          matchMap[index] = 2;
+        } else {
+          // was not right place, see if the guess letter exists at all
+          if (currentLetters.includes(gl)) {
+            // exists in pool, mark as correct-but-not-here
+            matchMap[index] = 1;
+          }
+        }
+      });
+    }
+
+    const letterBoxes: JSX.Element[] = matchMap.map((mm, index) => {
+      const matchClass = mm == 2 ? yesClass : (mm == 1 ? maybeClass : noClass);
+      return (<>
+        <span className={`letterBox ${matchClass}`}>{guessLetters[index].toLocaleUpperCase()}</span>
+      </>)
+    })
+
+    rows.unshift(
+      <div className='guessRow' key={`${guessId}-${guess}`}>
+        {isMatch ? "✅" : "❌"}: <span className="guessLetters">{letterBoxes.map((lb) => { return lb })}</span>
+      </div>
+    )
+  })
+
+  return <>
+    <div id="history">
+      {thepast.length > 0 && (<>{thepast.length} guesses<br /></>)}
+      <div id="historyRows">
+        {rows.map((r) => { return r })}
+      </div>
+    </div>
+  </>
+}
